60b506db621c868c57031f4423fcaea809df1daa
damianhaq <40294540+damianhaq@users.noreply.github.com>
2025-01-30 17:47:12 +0100

    draw cells

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import { GAME } from "./consts.js";
+import { GAME, ID } from "./consts.js";
 import { drawRectOnMap, Game } from "./DGamev3.js";
 
 const game = new Game();
-game.init("canvas", GAME.width, GAME.height, 2);
+game.init("canvas", GAME.width, GAME.height, 1);
 
-const map = createMap(10, 10, 15);
+const map = createMap(30, 30, 40);
+addIdToMap(map, 0.1, ID.tree);
 
 game.draw = function () {
   drawMap(map);
@@ -34,16 +35,46 @@ function createMap(cellsWidth, cellsHeight, cellLength, fill = 0) {
 function drawMap(map) {
   for (let y = 0; y < map.height; y++) {
     for (let x = 0; x < map.width; x++) {
-      //   console.log(map[y][x]);
+      // color depends on id
+
+      let color = null;
+      switch (map.arr[y][x]) {
+        case ID.stone:
+          color = "gray";
+          break;
+        case ID.grass:
+          color = "green";
+          break;
+        case ID.water:
+          color = "blue";
+          break;
+        case ID.tree:
+          color = "brown";
+          break;
+        default:
+          color = "white";
+      }
+
       drawRectOnMap(
-        x * map.cellLength,
-        y * map.cellLength,
-        map.cellLength,
-        map.cellLength,
+        x * map.cellLength + 1,
+        y * map.cellLength + 1,
+        map.cellLength - 2,
+        map.cellLength - 2,
         game.ctx,
         game.camera,
-        "#949494"
+        color,
+        1
       );
+    }
+  }
+}
+
+function addIdToMap(map, chance, id) {
+  for (let y = 0; y < map.height; y++) {
+    for (let x = 0; x < map.width; x++) {
+      if (Math.random() < chance) {
+        map.arr[y][x] = id;
+      }
     }
   }
 }
