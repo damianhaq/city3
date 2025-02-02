1e95113695b1a6cb886fb94685d9f009eb562fc6
damianhaq <40294540+damianhaq@users.noreply.github.com>
2025-02-02 09:53:17 +0100

    zoom map

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
-import { GAME, ID } from "./consts.js";
+import { GAME, RESOURCES } from "./consts.js";
 import { drawCircleOnMap, drawRectOnMap, Game } from "./DGamev3.js";
+import { Resource } from "./Resource.js";
 
-const game = new Game();
+export const game = new Game();
 game.init("canvas", GAME.width, GAME.height, 1);
 game.updateCamera(600, 400);
 
-const map = createMap(30, 30, 30);
-addIdToMap(map, 0.1, ID.tree);
-addIdToMap(map, 0.1, ID.stone);
-addIdToMap(map, 0.1, ID.water);
-addIdToMap(map, 0.1, ID.grass);
+export const map = createMap(30, 30, 20);
+
+const stone = new Resource(
+  "Stone",
+  RESOURCES.stone.id,
+  map,
+  game,
+  RESOURCES.stone.drawFunc
+);
+const tree = new Resource(
+  "Tree",
+  RESOURCES.tree.id,
+  map,
+  game,
+  RESOURCES.tree.drawFunc
+);
+const water = new Resource(
+  "Water",
+  RESOURCES.water.id,
+  map,
+  game,
+  RESOURCES.water.drawFunc
+);
+const grass = new Resource(
+  "Grass",
+  RESOURCES.grass.id,
+  map,
+  game,
+  RESOURCES.grass.drawFunc
+);
+
+addResourceToMap(map, 0.1, tree);
+addResourceToMap(map, 0.01, water);
+addResourceToMap(map, 0.1, grass);
+addResourceToMap(map, 0.1, stone);
+
+game.onMouseWheel = function (e) {
+  const zoomFactor = 10;
+
+  // ile kratek jest po lewej stronie ekranu
+  const cellsOnLeft = (game.camera.x + GAME.width / 2) / map.cellLength;
+  // ile kratek jest na górze ekranu
+  const cellsOnTop = (game.camera.y + GAME.height / 2) / map.cellLength;
+
+  // zoom in and out
+  if (e.deltaY < 0) {
+    map.cellLength += zoomFactor;
+    game.camera.x += zoomFactor * cellsOnLeft;
+    game.camera.y += zoomFactor * cellsOnTop;
+  } else {
+    map.cellLength -= zoomFactor;
+    game.camera.x -= zoomFactor * cellsOnLeft;
+    game.camera.y -= zoomFactor * cellsOnTop;
+  }
+};
 
 game.draw = function () {
   drawMap(map);
@@ -37,219 +88,61 @@ function createMap(cellsWidth, cellsHeight, cellLength, fill = 0) {
 }
 
 function drawMap(map) {
-  for (let y = 0; y < map.height; y++) {
-    for (let x = 0; x < map.width; x++) {
-      // color depends on id
-
-      let color = getColorById(map.arr[y][x]);
-
-      // draw empty cells
-      if (map.arr[y][x] === 0 && GAME.showGrid) {
-        drawRectOnMap(
-          x * map.cellLength + 1,
-          y * map.cellLength + 1,
-          map.cellLength - 2,
-          map.cellLength - 2,
-          game.ctx,
-          game.camera,
-          color,
-          1
-        );
-      } else if (map.arr[y][x] !== 0) {
-        // draw filled cells
-        drawRectOnMap(
-          x * map.cellLength + 1,
-          y * map.cellLength + 1,
-          map.cellLength - 2,
-          map.cellLength - 2,
-          game.ctx,
-          game.camera,
-          color,
-          1
-        );
-      }
-
-      // draw resources
-      drawResource(map.arr[y][x], color, x, y);
-    }
-  }
+  map.arr.forEach((row, y) => {
+    row.forEach((resource, x) => {
+      if (resource !== 0) resource.draw(x, y);
+    });
+  });
 }
 
-function drawTree(x, y) {
-  // internal constants
-  const trunkHeight = map.cellLength / 4;
-  const trunkWidth = map.cellLength / 8;
-  const foliageRadius = map.cellLength / 4;
+// function drawResource(id, color, x, y) {
+//   switch (id) {
+//     case ID.stone:
+//       drawStone(
+//         x * map.cellLength + map.cellLength / 2,
+//         y * map.cellLength + map.cellLength / 2
+//       );
+//       break;
+//     case ID.grass:
+//       drawGrass(
+//         x * map.cellLength + map.cellLength / 2,
+//         y * map.cellLength + map.cellLength / 2
+//       );
 
-  // adding camera position
-  x -= game.camera.x;
-  y -= game.camera.y;
+//       break;
+//     case ID.water:
+//       drawWater(
+//         x * map.cellLength + map.cellLength / 2,
+//         y * map.cellLength + map.cellLength / 2
+//       );
+//       break;
+//     case ID.tree:
+//       drawTree(
+//         x * map.cellLength + map.cellLength / 2,
+//         y * map.cellLength + map.cellLength / 2
+//       );
+//       break;
+//     default:
+//       break;
+//   }
+// }
 
-  // correction position
-  y -= 2;
+// function getColorById(id) {
+//   switch (id) {
+//     case ID.stone:
+//       return "#4f4f4f";
+//     case ID.grass:
+//       return "green";
+//     case ID.water:
+//       return "blue";
+//     case ID.tree:
+//       return "#199c1f";
+//     default:
+//       return "white";
+//   }
+// }
 
-  // Rysowanie pnia drzewa
-  game.ctx.fillStyle = "#8B4513"; // Kolor brązowy
-  game.ctx.fillRect(
-    x - trunkWidth / 2,
-    y - trunkHeight + map.cellLength / 2.5,
-    trunkWidth,
-    trunkHeight
-  );
-
-  // Rysowanie korony drzewa (liście)
-  game.ctx.beginPath();
-  game.ctx.arc(x, y, foliageRadius, 0, Math.PI * 2);
-  game.ctx.fillStyle = "#228B22"; // Kolor zielony
-  game.ctx.fill();
-}
-
-function drawStone(x, y) {
-  // internal constants
-  const stoneRadius = map.cellLength / 7;
-  // adding camera position
-  x -= game.camera.x;
-  y -= game.camera.y;
-
-  // correction position
-  y -= 2;
-
-  // Rysowanie kamienia
-  game.ctx.beginPath();
-  game.ctx.arc(x, y, stoneRadius, 0, Math.PI * 2);
-  game.ctx.fillStyle = "#606060FF"; // Kolor szary
-  game.ctx.fill();
-
-  // kolejny kanień
-  game.ctx.beginPath();
-  game.ctx.arc(
-    x + map.cellLength / 8,
-    y + map.cellLength / 8,
-    stoneRadius * 1.2,
-    0,
-    Math.PI * 2
-  );
-  game.ctx.fillStyle = "#4f4f4f"; // Kolor szary
-  game.ctx.fill();
-
-  // kolejny kanień
-  game.ctx.beginPath();
-  game.ctx.arc(
-    x - map.cellLength / 8,
-    y + map.cellLength / 8,
-    stoneRadius * 1.3,
-    0,
-    Math.PI * 2
-  );
-  game.ctx.fillStyle = "#585858FF"; // Kolor szary
-  game.ctx.fill();
-}
-
-function drawWater(x, y) {
-  // internal constants
-  const xStartOffset = -(map.cellLength / 4);
-  const xEndOffset = map.cellLength / 4;
-
-  // adding camera position
-  x -= game.camera.x;
-  y -= game.camera.y;
-
-  // correction position
-  // y -= 2;
-
-  game.ctx.strokeStyle = "#0000FF"; // Ciemnoniebieski kolor fal
-  game.ctx.lineWidth = 2;
-
-  game.ctx.beginPath();
-  game.ctx.moveTo(x + xStartOffset, y);
-
-  game.ctx.bezierCurveTo(
-    x,
-    y - map.cellLength / 4,
-    x,
-    y + map.cellLength / 4,
-    x + xEndOffset,
-    y
-  );
-  game.ctx.stroke();
-}
-
-function drawGrass(x, y) {
-  // adding camera position
-  x -= game.camera.x;
-  y -= game.camera.y;
-
-  // correction position
-  y += map.cellLength / 8;
-
-  game.ctx.strokeStyle = "#228B22"; // Zielony kolor trawy
-  game.ctx.lineWidth = 2;
-
-  const radius = map.cellLength / 4;
-
-  game.ctx.beginPath();
-  game.ctx.arc(x - radius, y, radius, 0, degToRad(300), true);
-  game.ctx.stroke();
-
-  // second blade of grass
-  game.ctx.beginPath();
-  game.ctx.arc(x + radius, y, radius, degToRad(180), degToRad(230));
-  game.ctx.stroke();
-}
-
-// angle to radians function
-function degToRad(deg) {
-  return (deg * Math.PI) / 180;
-}
-
-function drawResource(id, color, x, y) {
-  switch (id) {
-    case ID.stone:
-      drawStone(
-        x * map.cellLength + map.cellLength / 2,
-        y * map.cellLength + map.cellLength / 2
-      );
-      break;
-    case ID.grass:
-      drawGrass(
-        x * map.cellLength + map.cellLength / 2,
-        y * map.cellLength + map.cellLength / 2
-      );
-
-      break;
-    case ID.water:
-      drawWater(
-        x * map.cellLength + map.cellLength / 2,
-        y * map.cellLength + map.cellLength / 2
-      );
-      break;
-    case ID.tree:
-      drawTree(
-        x * map.cellLength + map.cellLength / 2,
-        y * map.cellLength + map.cellLength / 2
-      );
-      break;
-    default:
-      break;
-  }
-}
-
-function getColorById(id) {
-  switch (id) {
-    case ID.stone:
-      return "#4f4f4f";
-    case ID.grass:
-      return "green";
-    case ID.water:
-      return "blue";
-    case ID.tree:
-      return "#199c1f";
-    default:
-      return "white";
-  }
-}
-
-function addIdToMap(map, chance, id) {
+function addResourceToMap(map, chance, id) {
   for (let y = 0; y < map.height; y++) {
     for (let x = 0; x < map.width; x++) {
       if (Math.random() < chance) {
